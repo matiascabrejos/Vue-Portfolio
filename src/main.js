ea318001af7180b3f8e466a31f086fb456ffbdd8
matiascabrejos <matias.cabrejos@hotmail.com>
2021-11-22 21:31:43 -0300

    Setting up the project and testing things

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+import "./css/tailwind.css";
+import Testing from "./components/Testing.vue";
+import TheHome from "./components/TheHome.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/testing", component: Testing },
+    { path: "/", component: TheHome },
+  ],
+  linkActiveClass: "active",
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
