4e05903222231333935f9cdc77b3fe1573b98e4f
matiascabrejos <matias.cabrejos@hotmail.com>
2021-12-06 19:12:25 -0300

    Working on the project grid

--- src/components/Back.vue
+++ src/components/Back.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="bg-red-900 h-screen">AA</div>
+  <div class="h-screen">
+    <projects-grid></projects-grid>
+  </div>
 </template>
 
 <style scoped>
-div {
+/* div {
   background-color: #0e1118;
-}
+} */
 </style>
+
+<script>
+import ProjectsGrid from "./ProjectsGrid.vue";
+export default {
+  components: { ProjectsGrid },
+};
+</script>
